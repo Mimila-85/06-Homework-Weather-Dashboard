2b6910390fb56b6e43a4462e61eca10d94790021
Camila Alves Meyer <camila.alves85@gmail.com>
2020-11-04 15:45:52 +0800

    Refactored code with some arrow function, literals, let and const instead of var

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,30 +2,29 @@
 $(document).ready(function(){
     
     // Created global variables for classes in the html file that will constatly be used in this js file.
-    var searchArea = $(".searchArea");
-    var today = $(".today");
-    var forecast = $(".forecast");
+    const searchArea = $(".searchArea");
+    const today = $(".today");
+    const forecast = $(".forecast");
 
     // Created global variables for the API key that will be used in different AJAX calls.
-    var APIKey = "b37332257de420fc6dfcda2bbba28fbd";
+    const APIKey = "b37332257de420fc6dfcda2bbba28fbd";
 
     // Created a variable to change the API response units of measurement.
-    var imperialUnit = "&units=imperial";
+    const imperialUnit = "&units=imperial";
 
     // Created a global variable to store the city inputed from user.
-    var city ="";
+    let city;
 
     // Created an array to stored the cities returned from the API response.
-    var savedCity = [];
+    let savedCity = [];
     // console.log(savedCity);
 
     // Call initial function.    
-    initial()
-    
+    initial()    
     
     function initial(){
         // Created a variable to pull the cities stored in the local storage.
-        var storedCities = JSON.parse(localStorage.getItem("savedCity"));
+        const storedCities = JSON.parse(localStorage.getItem("savedCity"));
 
         // If there are cities stored in the local storage, then it will take place in the savedCity array.
         if (storedCities !== null){
@@ -36,7 +35,7 @@ $(document).ready(function(){
         createBtn();
 
         // Created variable to identify the last city researched by user, that is the last city pushed to the savedCity array.
-        var takeLastCity = savedCity[savedCity.length - 1];
+        const takeLastCity = savedCity[savedCity.length - 1];
 
         // Run function currentDay with the last city in the array to display the results of the last researched city.
         currentDay(takeLastCity);
@@ -48,7 +47,7 @@ $(document).ready(function(){
     }
     
     // Event listner to search button.
-    $(".searchBtn").on("click", function (event){
+    $(".searchBtn").on("click", event => {
         event.preventDefault();
         // Take value from input field that user entered. Trim to take any blank spaces at the end.
         city = $("#addCity").val().trim();
@@ -64,7 +63,7 @@ $(document).ready(function(){
     })
 
     // Event listner for the clean button. This gives an option to user to clean the previous searched cities.
-    $(".cleanBtn").on("click", function (event){
+    $(".cleanBtn").on("click", event => {
         event.preventDefault();
         // Clean local storage.
         localStorage.clear();
@@ -78,18 +77,18 @@ $(document).ready(function(){
     function currentDay(inputCity, createButton){
         
         // Variable that holds the API url along with variables key, unit and inputed city from user.
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?appid=" + APIKey + imperialUnit + "&q=" + inputCity;
+        const queryURL = "https://api.openweathermap.org/data/2.5/weather?appid=" + APIKey + imperialUnit + "&q=" + inputCity;
 
          
         // AJAX call for the specific criteria (city name) entered by the user.
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(response){            
+        }).then(response => {            
             // console.log(response);
 
             // Created variable to hold the city name response from API.
-            var cityName = response.name
+            const cityName = response.name
             // console.log(cityName);
 
             // If createButton boolean is true than create button; however, another if statement is entered, so if user types a name of a city that he/she searched before, then this city will not be entered in the savedCity array.
@@ -100,91 +99,78 @@ $(document).ready(function(){
                     }
             }
 
-            // Create a h2 tag with the city name.
-            var newh2 = $("<h2>").text(cityName);
-
             // Created variable to grab current day.
-            var d = new Date();
-
-            // Create a new span tag, and format the current day to be displayed as a text content.
-            var newH2Span = $("<span>").text(" (" + (d.getMonth()+1) + "/" + d.getDate() + "/" + d.getFullYear() + ")");
+            const d = new Date();
 
             // Created variable to grab the icon image name.
-            var dayIcon = response.weather[0].icon;
+            const dayIcon = response.weather[0].icon;
 
-            // Create an image tag. Add an attribute with the image address and add image name saved in variavle dayIcon.
-            var imgDayIcon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + dayIcon + ".png");
-            
-            // Append new span and image tag to the created h2 tag.
-            newh2.append(newH2Span, imgDayIcon);
-            
-            // Create a p tag to display the temperature response.
-            var ptemp = $("<p>").text("Temperature: " + response.main.temp + " ℉");
-
-            // Create a p tag to display the humidity response.
-            var phumidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
-            
-            // Create a p tag to display the wind speed response.
-            var pwindSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + " mph");
-            
+            // Create new elements to be append in the html file using the API response as part of the text.
+            const newCurrentWeather = `
+            <h2>${cityName}<span>(${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()})</span>
+            <img src="https://openweathermap.org/img/wn/${dayIcon}.png"></img>
+            </h2>
+            <p>Temperature: ${response.main.temp} ℉</p>
+            <p>Humidity: ${response.main.humidity}%</p>
+            <p>Wind Speed: ${response.wind.speed} mph;
+            <p class="uv">UV Index: </p>
+            <img class='imgUv' src="https://www.epa.gov/sites/production/files/sunwise/images/uviscaleh_lg.gif" alt="UV Color Scale"></img>
+            <br>
+            <br>
+            `
             // Created varibles to store the latitude and longitude responses that are going to be used in the URL to grab the UV.
-            var latitude = response.coord.lat            
-            var long = response.coord.lon
-            
-            // Create a p tag to display the UV response.
-            var pUV = $("<p>").text("UV Index: ");
+            const latitude = response.coord.lat            
+            const long = response.coord.lon
             
             // Calls the function that runs the AJAX with the UV API.
             getUV();
 
-            var imageUV = $("<img class='imgUv'>").attr("src", "https://www.epa.gov/sites/production/files/sunwise/images/uviscaleh_lg.gif");
-
             // Clears the div with the today class, and add the response for the new city searched.
-            today.empty().append(newh2, ptemp, phumidity, pwindSpeed, pUV, imageUV);
-
-            
+            today.empty().append(newCurrentWeather);            
 
             function getUV(){
 
-                // Created variable to hold API url along with  key, and the searched city latitute and longitude.
-                var uvURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + long + "&appid=" + APIKey
+                // Created variable to hold API url along with key, and the searched city latitute and longitude.
+                const uvURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + long + "&appid=" + APIKey
 
-                    // AJAX call function for to get the UV API response.
-                    $.ajax({
-                        url: uvURL,
-                        method: "GET"
-                    }).then(function(resUV){
-                        // console.log(resUV);
-                        
-                        // Created variable to hold UV response.
-                        var uv = resUV.value;
+                // AJAX call function for to get the UV API response.
+                $.ajax({
+                    url: uvURL,
+                    method: "GET"
+                }).then(resUV => {
+                    // console.log(resUV);
+                    
+                    // Created variable to hold UV response.
+                    const uv = resUV.value;
 
-                        // Created a span tag with the UV response.
-                        var spanUV = $("<span>").text(uv);
+                    $(".spUv").remove();
 
-                        // Append the span tag to the p tag created previously.
-                        pUV.append(spanUV);
-                        
-                        // Gets the UV response and turns into a number.
-                        var parsenUV = parseInt(uv);
+                    // Created a span tag with the UV response.
+                    const spanUV = $("<span class='spUv'>").text(uv);
 
-                        // The following if statements are to color code the UV level according to https://www.epa.gov/sunsafety/uv-index-scale-0
-                        if (parsenUV <= 2){
-                            spanUV.attr("style", "background: green")
-                        }
-                        else if (parsenUV >= 3 && parsenUV <= 5){
-                            spanUV.attr("style", "background: yellow")
-                        }
-                        else if (parsenUV === 6 || parsenUV === 7){
-                            spanUV.attr("style", "background: orange")
-                        }
-                        else if (parsenUV >= 8 && parsenUV <= 10){
-                            spanUV.attr("style", "color: white;background: red")
-                        }
-                        else if (parsenUV >= 11){
-                            spanUV.attr("style", "color: white;background: purple")
-                        }
-                    })
+                    // Append the span tag to the p tag created previously.
+                    $(".uv").append(spanUV);
+                    
+                    // Gets the UV response and turns into a number.
+                    const parsenUV = parseInt(uv);
+
+                    // The following if statements are to color code the UV level according to https://www.epa.gov/sunsafety/uv-index-scale-0
+                    if (parsenUV <= 2){
+                        spanUV.attr("style", "background: green")
+                    }
+                    else if (parsenUV >= 3 && parsenUV <= 5){
+                        spanUV.attr("style", "background: yellow")
+                    }
+                    else if (parsenUV === 6 || parsenUV === 7){
+                        spanUV.attr("style", "background: orange")
+                    }
+                    else if (parsenUV >= 8 && parsenUV <= 10){
+                        spanUV.attr("style", "color: white;background: red")
+                    }
+                    else if (parsenUV >= 11){
+                        spanUV.attr("style", "color: white;background: purple")
+                    }
+                })
             }
 
             // Call function that displays 5 days forecast.
@@ -193,68 +179,53 @@ $(document).ready(function(){
             function futureDates(){
                 
                 // Created variable to hold the forecast URL, adding the user search city name, API key, and unit change from standard to imperial.
-                var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey + imperialUnit
+                const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey + imperialUnit
 
                 // AJAX call for the specific city search by the user.
                 $.ajax({
                     url: forecastURL,
                     method: "GET"
-                }).then(function(forecastRes){
+                }).then(forecastRes => {
                     // console.log(forecastRes);
 
                     // Created varibale that holds the API response with a list of the every three works forecast for a five days period.
-                    var forecastArray = forecastRes.list
+                    const forecastArray = forecastRes.list
 
                     // Create a h3 tag to display the 5-Day Forecast title.
-                    var newForH3 = $("<h3>").text("5-Day Forecast");
+                    const newForH3 = `<h3>5-Day Forecast</h3>`;
                     
                     // Create a new div with a bootstrap class card-deck.
-                    var newCardDeck = $("<div class='card-deck'>");
+                    const newCardDeck = `<div class="card-deck"></div>`;
 
                     // Empty the div with class forecast, then append the new h3 and the new div.
                     forecast.empty().append(newForH3, newCardDeck);
 
                     // Create a for loop to loop through the forecast array. Start to loop at index 7 to skip the current day. The response comes back in 3 hours range; therefore, each returns 8 result per day, that's why i is added by 8 each time, to take a new day.
-                    for (var i=7; i < forecastArray.length; i+=8){
-
-                        // Create a new div with bootstrap class card-body for each result.
-                        var newDivCardBody = $("<div class='card-body'>");
+                    for (let i=7; i < forecastArray.length; i+=8){
 
                         // Created variable to store date from response and format it to JavaScript.
-                        var forDate = new Date(forecastArray[i].dt_txt);
+                        const forDate = new Date(forecastArray[i].dt_txt);
                         // console.log(forDate);
 
-                        // Created a new p tag to hold the date, as well as format it to be displayed.
-                        var pForDate = $("<p>").text(forDate.getMonth()+1 + "/" + forDate.getDate() + "/" + forDate.getFullYear());
-
                         // Created variable to hold each icon name.
-                        var forDayIcon = forecastArray[i].weather[0].icon;
+                        const forDayIcon = forecastArray[i].weather[0].icon;
 
-                        // Create a new img tag, and add an attribute with the image address along with the icon name held in the forDayIcon variable.
-                        var imgForDayIcon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + forDayIcon + ".png");
-                        
-                        // Create p tage to display the temperature.
-                        var pForTemp = $("<p>").text("Temperature: " + forecastArray[i].main.temp + " ℉");
-                        
-                        // Create p tag to display humidity.
-                        var pForHumidity = $("<p>").text("Humidity: " + forecastArray[1].main.humidity + "%");  
-                        
-                        // Append the new p tags and image tag to the div with class card-body.
-                        newDivCardBody.append(pForDate, imgForDayIcon, pForTemp, pForHumidity);
-
+                        // Create a new div with bootstrap class card-body for each result.
+                        const newDivCardBody = `<div class="card-body">
+                        <p>${forDate.getMonth()+1}/${forDate.getDate()}/${forDate.getFullYear()}
+                        <img src="https://openweathermap.org/img/wn/${forDayIcon}.png"></img>
+                        <p>Temperature: ${forecastArray[i].main.temp} ℉<p>
+                        <p>Humidity: ${forecastArray[1].main.humidity}%</p>
+                        </div>  
+                        `
                         // Append the div with class card-body to the div with class card-deck.
-                        newCardDeck.append(newDivCardBody);
-                    }
-                      
+                        $(".card-deck").append(newDivCardBody);
+                    }                      
                 })
-
             }
-
             // Call create button function.
             createBtn();   
-
-        })
-            
+        })            
     }
 
     // Event listern for the buttons created with the city names searched by the user.
@@ -274,24 +245,16 @@ $(document).ready(function(){
         $(".cityBtn").remove();   
 
         // Create new button for each element inside the savedCity array.
-        savedCity.forEach(function(cn){
+        savedCity.forEach(cn => {
             // console.log(cn);            
             
             // Create a new button with the city names inside the savedCity array.
-            var newCityBtn = $("<button>").text(cn);
+            const newCityBtn = `<button class="cityBtn btn-block" data-name="${cn}">${cn}</button>`
 
-            // Add a data-name attribute with the city name.
-            newCityBtn.attr("data-name", cn);
-
-            // Add classes to the new button.
-            newCityBtn.addClass("cityBtn btn-block");
-            
             // Append the new button tag to the div with class searchArea.
             searchArea.append(newCityBtn);
         })
         // Call storeCities function.
         storeCities();
     }
-    
-        
 })
